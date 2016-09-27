aacecb23027780abd79911eda4a6393ff2b71bcc
sharp.maestro <sharp.maestro@gmail.com>
2016-09-27 20:13:04 +0100

    Add shannon entropy calculating function

--- src/clj_entropy_calc/core.clj
+++ src/clj_entropy_calc/core.clj
@@ -1,6 +1,20 @@
 (ns clj-entropy-calc.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn log2 [n]
+  (/ (Math/log n) (Math/log 2)))
+
+
+(defn calc-shannon [in-str]
+  (let [char-freqs (vals (frequencies in-str))
+        strlen     (count in-str)
+        char-probs (map (fn [letter-count]
+                          (/ letter-count strlen))
+                        char-freqs)
+        char-logs (map log2 char-probs)
+        char-products (map * char-logs char-probs)]
+  (* -1 (reduce + char-products))))
+
+;(calc-shannon "1223334444")
+
+;(calc-shannon "int main(int argc, char *argv[])")
